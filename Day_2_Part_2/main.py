dd83fe4e64e15bd4375dbe1d9fb3bebe6ca60058
mikusmichal <mikusmichal@email.cz>
2018-12-06 22:59:00 +0100

    Day 2 part 2 - completed without Levenshtein

--- Day_2_Part_2/main.py
+++ Day_2_Part_2/main.py
@@ -1,20 +1,27 @@
-from Levenshtein import *
 if __name__ == "__main__":
 
     words = []
-    answer = ["dude", "dwde"]
+    answer = []
 
     def search():
+        counter = 0
         with open('input.txt') as lines:
             for line in lines:
                 words.append(line)
 
         for word1 in words:
             for word2 in words:
-                if Levenshtein.distance(word1, word2) == 1:
+                for letter1, letter2 in zip(word1, word2):
+                    if letter1 != letter2:
+                        counter += 1
+
+                if counter == 1:
                     answer.append(word1)
                     answer.append(word2)
-                    return
+                    print("The answer is:" + str(answer))
+                    return cut()
+                counter = 0
+
 
     def cut():
         word = answer[0]
@@ -23,9 +30,8 @@ if __name__ == "__main__":
         for letter1, letter2 in zip(word, word2):
             if letter1 == letter2:
                 cut_word += letter1
-        print(cut_word)
+        return cut_word
 
-search()
-cut()
+    print(search())
 
 
